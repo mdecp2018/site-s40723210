4de417c59f7218bbb32661466c1c66ee8d57a862
s40723210 <40723210@gm.nfu.edu.tw>
2018-10-23 23:33:41 +0800

    music

--- flaskapp.py
+++ flaskapp.py
@@ -1918,6 +1918,7 @@ window.location= 'https://' + location.host + location.pathname + location.searc
 <li><a href="sitemap.html">Site Map</a></li>
 <li><a href="./../reveal/index.html">reveal</a></li>
 <li><a href="./../blog/index.html">blog</a></li>
+<li><a href="./../music/index.html">music</a></li>
 '''
     outstring += '''
 </ul>
